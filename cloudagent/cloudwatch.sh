0f54bf0a4e20c55e9c29606c3e9eb2fbd0cae0eb
krishkrishna707 <62055882+krishkrishna707@users.noreply.github.com>
2022-10-20 14:25:20 +0530

    Update cloudwatch.sh

--- cloudagent/cloudwatch.sh
+++ cloudagent/cloudwatch.sh
@@ -10,3 +10,14 @@
  mkdir -p /usr/share/collectd/
  touch /usr/share/collectd/types.db
  sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c ssm:ubuntu
+
+
+
+linux
+
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+sudo rpm -U ./amazon-cloudwatch-agent.rpm
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard
+mkdir -p /usr/share/collectd/
+touch /usr/share/collectd/types.db
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c ssm:Amazoncloudwatch-linux
